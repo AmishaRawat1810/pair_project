323b7bc788cf6edbbc6939a04ad67ed5bfcac3d3
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-02-09 10:37:51 +0530

    setup

---
+++ test/main_test.js
@@ -0,0 +1,10 @@
+import { assertEquals } from "jsr:@std/assert";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { main } from "../src/candy_crush.js";
+
+describe("Simple test", () => {
+  it("Value equals 1", () => {
+      const result = main();
+      assertEquals(result, 1)
+    })
+  })
